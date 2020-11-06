9d5a3e6309c06e8b16c683165ad153d75bfc2c74
GimYoungPhil <youngphil.gim@gmail.com>
2020-11-06 17:04:27 +0900

    fixed

--- src/ch2/cipher.ts
+++ src/ch2/cipher.ts
@@ -1,5 +1,3 @@
-
-
 type Alphabet = "A" | "B" | "C" | "D" | "E" | "F"
               | "G" | "H" | "I" | "J" | "K" | "L"
               | "M" | "N" | "O" | "P" | "Q" | "R"
@@ -16,9 +14,11 @@ type CiphersDecimal = ReadonlyArray<Decimal>;
 
 type CiphersArray = CiphersAlphabet | CiphersDecimal;
 
+// type CiphersArray = ReadonlyArray<Alphabet | Decimal>;
+
 
 // 암호 문자
-const CIPHERS_ALPHABET: CiphersArray = [
+const CIPHERS_ALPHABET: CiphersAlphabet = [
   'A', 'B', 'C', 'D', 'E', 'F',
   'G', 'H', 'I', 'J', 'K', 'L',
   'M', 'N', 'O', 'P', 'Q', 'R',
@@ -27,7 +27,7 @@ const CIPHERS_ALPHABET: CiphersArray = [
 ];
 
 // 암호 문자
-const CIPHERS_DECIMAL: CiphersArray = [
+const CIPHERS_DECIMAL: CiphersDecimal = [
   0,  1,  2,  3,  4,  5,
   6,  7,  8,  9, 10, 11,
  12, 13, 14, 15, 16, 17,
